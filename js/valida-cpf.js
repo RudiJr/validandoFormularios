9575b8d75b4d2abb3d29cc7055a55b6e0fd50a85
RudiJr <rudi.juniior@gmail.com>
2023-06-14 12:21:27 -0300

    Created validaNumeroRepetidos function

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -4,7 +4,24 @@ export default function validoCPF(campo){
     `g` flag means that it will replace all occurrences of the pattern, not just the
     first one. */
     const cpf = campo.value.replace(/\.|-/g, ""); 
-    console.log(cpf)
+    validaNumerosRepetidos(cpf);
 };
 
+function validaNumerosRepetidos(cpf){
+    // percorre a lista, caso cpf seja encontrado retorna TRUE caso contrario retorna FALSE
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numerosRepetidos.includes(cpf);
+};
 
