3f25a45300d506820c4d3fed6cab7e6730d7ab10
RudiJr <rudi.juniior@gmail.com>
2023-06-15 12:01:22 -0300

    Created validaIdade function

--- js/valida-idade.js
+++ js/valida-idade.js
@@ -1,4 +1,19 @@
 export default function maiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
-    console.log(dataNascimento);
+    validaIdade(dataNascimento);
+
+    console.log(validaIdade(dataNascimento))
+};
+
+
+/* `getUTCFullYear()` is a method of the `Date` object in
+    JavaScript that returns the year of the specified date
+    according to universal time. It returns a four-digit number
+    representing the year of the date in the UTC time zone. */
+    
+function validaIdade(data){
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataAtual >= dataMais18;
 };
